6eda9f6ac386abfd5ab3c890d0dde8127f298dbe
Zubair <zubair99ss@hotmail.com>
2024-03-10 22:21:05 +0800

    Complete menu content

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -13,26 +13,39 @@ function menuContent() {
     menuContainer.classList.add('flex');
     menuContainer.setAttribute('id', 'menu__container');
 
-    
+    // Set menu items
+    [
+        { id: 'CoqAuVin', name: 'Coq Au Vin', ingredients: 'Chicken, Red Wine, Bacon, Mushroos and Onion', price: '$18.50' },
+        { id: 'Boeuf__Bourguignon', name: 'Boeuf Bourguignon', ingredients: 'Beef, Red Wine, Bacon, Carrots, Onion', price: '$21' },
+        { id: 'Ratatouille', name: 'Ratatouille', ingredients: 'Eggplant, Zubcchini, Bell Peppers, Tomatoes, Onion', price: '$18' },
+        { id: 'Bouillabaisse', name: 'Bouillabaisse', ingredients: 'Fish, Tomatoes, Onion, Garlic, Saffron', price: '$24.50' },
+        { id: 'Salade__Nicoise', name: 'Salade Nicoise', ingredients: 'Lettuce, Tuna, Hard Boiled Eggs, Tomatoes, Green Beams', price: '$22'}
+    ].forEach(item => {
+        const menuItemCard = document.createElement('div');
+        menuItemCard.classList.add('menu__item');
+        menuContainer.appendChild(menuItemCard);
+        menuItemCard.setAttribute('id', item.id);
 
-    // Create menu items
-    for(var i = 0; i < 5; i++) {
-        const menuItem = document.createElement('div');
-        menuItem.classList.add('menu__item');
-        menuContainer.appendChild(menuItem);
+        const menuItemImage = document.createElement('div');
+        menuItemCard.appendChild(menuItemImage);
+        menuItemImage.classList.add('menu__item__image');
+        menuItemImage.setAttribute('id', item.id + '__image');
 
-        const itemName = document.createElement('p');
-        menuItem.appendChild(itemName);
-    }
-    const menuItems = menuContainer.getElementsByTagName('div');
+        const menuItemName = document.createElement('p');
+        menuItemCard.appendChild(menuItemName);
+        menuItemName.classList.add('item__name');
+        menuItemName.textContent = item.name;
 
-    // Set id's of each menu item
-    menuItems[0].setAttribute('id', 'CoqAuVin');
-    menuItems[1].setAttribute('id', 'Boeuf Bourguignon');
-    menuItems[2].setAttribute('id', 'Ratatouille');
-    menuItems[3].setAttribute('id', 'Bouillabaisse');
-    menuItems[4].setAttribute('id', 'Salade Nicoise');
-    
+        const menuItemIngredients = document.createElement('p');
+        menuItemCard.appendChild(menuItemIngredients);
+        menuItemIngredients.classList.add('ingredients');
+        menuItemIngredients.textContent = item.ingredients;
+
+        const menuPrice = document.createElement('span');
+        menuItemCard.appendChild(menuPrice);
+        menuPrice.classList.add('item__price');
+        menuPrice.textContent = item.price;
+    });    
 }
 
 export default menuContent;
